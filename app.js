62c6df9357919927be11823b720e362a6a4bab91
minhaz11 <minhaz217@gmail.com>
2021-09-14 09:36:00 +0600

    Finalize

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-var firstClassPrice = 150;
-var economyPrice = 100;
-var vat = 10;
-
 var subTotal = document.getElementById('sub-total');
 var total = document.getElementById('total');
 var vatElement = document.getElementById('vat');
 
-function firstClassClick(minus = false) { 
-    var firstClass = document.getElementById('first-class');
+function clickHandler(id,minus = false) { 
+    var element = document.getElementById(id);
 
     if(minus){
-        if(firstClass.value > 0){
-            firstClass.value  = parseInt(firstClass.value || 0) - 1;
-            totalCalculation(firstClass.value,true)
+        if(element.value > 0){
+            element.value  = parseInt(element.value) - 1;
+            totalCalculation(id,true)
         } else{
-            firstClass.value = 0;
+            element.value = 0;
         }
          
     } else{
-        firstClass.value  = parseInt(firstClass.value || 0)+1; 
-        totalCalculation(firstClass.value)
-    }
-
-    function totalCalculation(value,minus = false){
-
-        if(minus){
-            subTotal.innerText = parseFloat(subTotal.innerText) - (firstClassPrice * value);
-        } else {
-            subTotal.innerText = parseFloat(subTotal.innerText) + (firstClassPrice * value);
-        }
-
-        var vatCalc =  parseFloat(subTotal.innerText) * vat/100
-    
-        vatElement.innerText = vatCalc;
-
-        total.innerText =  parseFloat(subTotal.innerText) + vatCalc;
-       
+        element.value  = parseInt(element.value)+1; 
+        totalCalculation(id)
     }
 
 }
+
+
+
+function totalCalculation(id,minus = false){
+
+    if(id == 'first-class'){
+        var price = 150;
+    } else {
+        var price = 100;
+    }
+
+    const vat = 10;
+
+    if(minus){
+        subTotal.innerText = parseFloat(subTotal.innerText) - price;
+    } else {
+        subTotal.innerText = parseFloat(subTotal.innerText) + price ;
+    }
+
+    var vatCalc =  parseFloat(subTotal.innerText) * vat/100
+
+    vatElement.innerText = vatCalc;
+
+    total.innerText =  parseFloat(subTotal.innerText) + vatCalc;
+}
+
+
+var button = document.getElementById('book');
+button.addEventListener('click',function () { 
+  
+    if(total.innerText == 0){
+
+       alert('Please select the ticket quantity first.')
+
+    } else{
+        alert(
+            `Thank you for being with us. Total Price is $${total.innerText} including 10% VAT.`
+            
+        )
+    }
+})
